b70fd810c0650bc6b76bdff3681a922dc68ee71d
denis.troller <denis.troller@sonarsource.com>
2024-10-18 11:48:32 +0200

    add S3649

---
+++ BlazorAppSecurityTests/Class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppSecurityTests
+{
+    public class UserDBContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+
+    }
+}
